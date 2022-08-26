1f26642dbb3abd514c8e41576282fd0d7c0ff792
Eяw!и Gontier <ierwinx@me.com>
2022-08-26 17:21:56 -0500

    Initial Commit

---
+++ UsersApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UsersApp
+//
+//  Created by Erwin Luz León on 26/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UsersApp/UsersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersAppApp.swift
+//  UsersApp
+//
+//  Created by Erwin Luz León on 26/08/22.
+//
+
+import SwiftUI
+
+@main
+struct UsersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
